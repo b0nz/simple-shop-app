0a26bc6d133d3dee3765082289dde9a5e15df69c
b0nz <elliantosarono.p@gmail.com>
2020-05-02 13:52:51 +0700

    handle update data

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -107,9 +107,16 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  void updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = 'https://helloworld-210608.firebaseio.com/products/$id.json';
+      await http.patch(url, body: json.encode({
+        'title': newProduct.title,
+        'description': newProduct.description,
+        'imageUrl': newProduct.imageUrl,
+        'price': newProduct.price,
+      }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
